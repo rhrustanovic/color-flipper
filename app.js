42b1419eb7cfeed6f9f7ac63047a0d8b4cecda47
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2022-01-31 00:15:28 +0100

    color flipper

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "blue", "rgba(99,166,220)", "#c26493", "pink"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", function () {
+    // get random number between 0 - 3 color
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
